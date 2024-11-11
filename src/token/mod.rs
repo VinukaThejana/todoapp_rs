d75d328035d18ff61b708fa105bbb64a61407780
Vinuka Kodituwakku <vinuka.t@pm.me>
2024-11-11 18:35:34 +0530

    feat(token): added token types

--- src/token/mod.rs
+++ src/token/mod.rs
@@ -1,4 +1,30 @@
+use std::fmt::Display;
+
 pub mod claims;
 pub mod error;
 pub mod traits;
 pub mod types;
+
+pub enum TokenType {
+    Access,
+    Refresh,
+    Session,
+}
+
+impl Display for TokenType {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        let token: &str = match *self {
+            TokenType::Access => "access_token",
+            TokenType::Refresh => "refresh_token",
+            TokenType::Session => "session_token",
+        };
+
+        write!(f, "{token}")
+    }
+}
+
+impl TokenType {
+    pub fn get_key(&self, jti: &str) -> String {
+        format!("{}:{}", self, jti)
+    }
+}
