158335fa6b4c4ddecc6250f7804111fe9ff94cb9
Vinuka Kodituwakku <vinuka.t@pm.me>
2024-11-16 00:08:49 +0530

    chore(env): added proper error documentation

--- src/config/env.rs
+++ src/config/env.rs
@@ -20,37 +20,65 @@ pub struct Env {
     #[serde(deserialize_with = "deserialize_arc_str")]
     pub redis_url: Arc<str>,
 
-    #[validate(length(min = 1, message = "refresh token private key must be provided"))]
+    #[validate(length(
+        min = 1,
+        message = "refresh token private key is required and cannot be empty"
+    ))]
     #[serde(deserialize_with = "deserialize_base64")]
     pub refresh_token_private_key: Arc<Vec<u8>>,
 
-    #[validate(length(min = 1, message = "refresh token public key must be provided"))]
+    #[validate(length(
+        min = 1,
+        message = "refresh token public key is required and cannot be empty"
+    ))]
     #[serde(deserialize_with = "deserialize_base64")]
     pub refresh_token_public_key: Arc<Vec<u8>>,
 
-    #[validate(length(min = 1, message = "access token private key must be provided"))]
+    #[validate(length(
+        min = 1,
+        message = "access token private key is required and cannot be empty"
+    ))]
     #[serde(deserialize_with = "deserialize_base64")]
     pub access_token_private_key: Arc<Vec<u8>>,
 
-    #[validate(length(min = 1, message = "access token public key must be provided"))]
+    #[validate(length(
+        min = 1,
+        message = "access token public key is required and cannot be empty"
+    ))]
     #[serde(deserialize_with = "deserialize_base64")]
     pub access_token_public_key: Arc<Vec<u8>>,
 
-    #[validate(length(min = 1, message = "session token private key must be provided"))]
+    #[validate(length(
+        min = 1,
+        message = "session token private key is required and cannot be empty"
+    ))]
     #[serde(deserialize_with = "deserialize_base64")]
     pub session_token_private_key: Arc<Vec<u8>>,
 
-    #[validate(length(min = 1, message = "session token public key must be provided"))]
+    #[validate(length(
+        min = 1,
+        message = "session token public key is required and cannot be empty"
+    ))]
     #[serde(deserialize_with = "deserialize_base64")]
     pub session_token_public_key: Arc<Vec<u8>>,
 
-    #[validate(range(min = 1, message = "refresh token expiration must be greater than 0"))]
+    #[validate(range(
+        min = 172_800,
+        message = "refresh token expiration must be greater than 172,800 seconds (2 Days)"
+    ))]
     pub refresh_token_expiration: usize,
 
-    pub session_token_expiration: usize,
-
-    #[validate(range(min = 1, message = "access token expiration must be greater than 0"))]
+    #[validate(range(
+        max = 86_400,
+        message = "access token expiration must be smaller than 86,400 seconds (1 Day)"
+    ))]
     pub access_token_expiration: usize,
+
+    #[validate(range(
+        min = 172_800,
+        message = "session token expiration must be greater than 172,800 seconds (2 Days)"
+    ))]
+    pub session_token_expiration: usize,
 
     #[validate(range(
         min = 8080,
@@ -95,13 +123,13 @@ impl Env {
                     }
                 });
 
-            println!("\nValidation errors");
-            println!("{}", message);
-            println!("\nUpdate the .env file to resolve the above errors and try again");
+            eprintln!("\nValidation errors");
+            eprintln!("{}", message);
+            eprintln!("\nUpdate the .env file to resolve the above errors and try again");
             std::process::exit(1);
         });
-        env_logger::init();
 
+        env_logger::init();
         env
     }
 }
