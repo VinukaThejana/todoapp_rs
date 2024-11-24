d39a223630b083b166bddde2ba9e866145a7d52e
Vinuka Kodituwakku <vinuka.t@pm.me>
2024-11-24 14:30:23 +0530

    feat(database): added the password hasing logic

--- src/database/user.rs
+++ src/database/user.rs
@@ -1,19 +1,15 @@
 use crate::{
     entity::{prelude::User, user},
-    model::user::UpdateUser,
+    model::user::{CreateUserReq, UpdateUserReq},
 };
 use sea_orm::*;
 
-pub async fn create(
-    email: String,
-    name: String,
-    password: String,
-    db: &DatabaseConnection,
-) -> Result<user::Model, DbErr> {
+pub async fn create(user: CreateUserReq, db: &DatabaseConnection) -> Result<user::Model, DbErr> {
     User::insert(user::ActiveModel {
-        email: Set(email),
-        name: Set(name),
-        password: Set(password),
+        email: Set(user.email),
+        name: Set(user.name),
+        password: Set(bcrypt::hash(user.password, bcrypt::DEFAULT_COST)
+            .map_err(|err| DbErr::Custom(err.to_string()))?),
         ..Default::default()
     })
     .exec_with_returning(db)
@@ -34,7 +30,7 @@ pub async fn find_by_email(
         .await
 }
 
-pub async fn update(data: UpdateUser, db: &DatabaseConnection) -> Result<user::Model, DbErr> {
+pub async fn update(data: UpdateUserReq, db: &DatabaseConnection) -> Result<user::Model, DbErr> {
     let mut update = user::ActiveModel {
         id: Set(data.id),
         ..Default::default()
@@ -47,7 +43,8 @@ pub async fn update(data: UpdateUser, db: &DatabaseConnection) -> Result<user::M
         update.name = Set(name);
     }
     if let Some(password) = data.password {
-        update.password = Set(password);
+        update.password = Set(bcrypt::hash(password, bcrypt::DEFAULT_COST)
+            .map_err(|err| DbErr::Custom(err.to_string()))?);
     }
 
     update.save(db).await?.try_into_model()
